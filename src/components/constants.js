04f142c23a216c27c67d742e7c611d8aaade408c
Delta Student <hp3432884@gmail.com>
2026-02-05 12:28:36 +0530

    changes

--- src/components/constants.js
+++ src/components/constants.js
@@ -27,18 +27,9 @@ import postmanLogo from '../assets/tech_logo/postman.png';
 import vercelLogo from '../assets/tech_logo/vercel.png';
 
 
-// Experience Section Logos
-import webverseLogo from '../assets/company_logo/webverse_logo.png';
-import agcLogo from '../assets/company_logo/agc_logo.png';
-import newtonschoolLogo from '../assets/company_logo/newtonschool_logo.png';
-
-// Education Section Logos
-import glaLogo from '../assets/education_logo/gla_logo.png';
-import bsaLogo from '../assets/education_logo/bsa_logo.png';
-import vpsLogo from '../assets/education_logo/vps_logo.png';
-
 // Project Section Logos
 import airbnblogo from '../assets/work_logo/airbnb.png';
+import myportfolio from '../assets/work_logo/myportfolio.png';
 
 export const SkillsInfo = [
   {
@@ -83,45 +74,7 @@ export const SkillsInfo = [
 ];
 
  
-  export const education = [
-    {
-      id: 0,
-      img: glaLogo,
-      school: "GLA University, Mathura",
-      date: "Sept 2022 - July 2024",
-      grade: "7.81 CGPA",
-      desc: "I have completed my Master's degree (MCA) in Computer Applications from GLA University, Mathura. During my time at GLA, I gained a strong foundation in programming, software development, and computer science principles. I have studied courses such as Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Web Development, and Software Engineering. I actively participated in various workshops and technical events, which enhanced my skills and knowledge. My experience at GLA University has been instrumental in shaping my technical abilities and professional growth.",
-      degree: "Master of Computer Applications - MCA",
-    },
-    {
-      id: 1,
-      img: bsaLogo,
-      school: "BSA College, Mathura",
-      date: "Sept 2018 - Aug 2021",
-      grade: "73.2%",
-      desc: "I completed my Bachelor's degree in Computer Science (B.Sc.) from BSA College, Mathura. Throughout my studies, I was immersed in a variety of subjects that deepened my understanding of computing and technology. From exploring Data Structures and Algorithms to diving into Web Development and Database Management Systems, I gained practical insights into the world of software development. My time at BSA College allowed me to work on projects that applied theoretical concepts to real-world problems.",
-      degree: "Bachelor of Science - BSC (Computer Science)",
-    },
-    {
-      id: 2,
-      img: vpsLogo,
-      school: "Vatsalya Public School Govardhan, Mathura",
-      date: "Apr 2017 - March 2018",
-      grade: "78%",
-      desc: "I completed my class 12 education from Vatsalya Public School, Govardhan, under the CBSE board, where I studied Physics, Chemistry, and Mathematics (PCM) with Computer Science.",
-      degree: "CBSE(XII) - PCM with Computer Science",
-    },
-    {
-      id: 3,
-      img: vpsLogo,
-      school: "Vatsalya Public School Govardhan, Mathura",
-      date: "Apr 2015 - March 2016",
-      grade: "87.5%",
-      desc: "I completed my class 10 education from Vatsalya Public School, Govardhan, under the CBSE board, where I studied Science with Computer.",
-      degree: "CBSE(X), Science with Computer Application",
-    },
-  ];
-  
+ 
   export const projects = [
     {
       id: 0,
@@ -132,6 +85,16 @@ export const SkillsInfo = [
       tags: ["HTML", "CSS", "JavaScript","EJS","Bootstrap", "React JS","Node.js","Express.js","MongoDB","MVC Architecture","REST API","Authentication","Authorization",],
       github: "https://github.com/hariom-p1306/AirBnb-Full-Stack-Project-.git",
       webapp: "https://airbnb-full-stack-project-e7eq.onrender.com/listings",
+    },
+      {
+      id: 1,
+      title: "Hariom Patel – Developer Portfolio",
+      description:
+       "A modern and responsive portfolio website showcasing the projects, skills, and achievements of Hariom Patel, a passionate Full Stack Developer. Built with React, TailwindCSS, and JavaScript, featuring interactive UI components, smooth scrolling, and a contact form integrated with EmailJS. Designed to demonstrate technical skills, creativity, and professional experience.",
+      image: myportfolio,
+      tags: ["HTML","React ","JavaScript","TailwindCSS"],
+      github: "https://github.com/hariom-p1306/My-Portfolio.git",
+      webapp: "https://my-portfolio-three-ivory-72.vercel.app/",
     },
     // {
     //   id: 1,
