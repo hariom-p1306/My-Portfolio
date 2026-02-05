3e4fed1265dfe6aedfbf69fd51057b2bba644e7a
Delta Student <hp3432884@gmail.com>
2026-02-05 11:22:43 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import About from "./components/About/About";
 import Skills from "./components/Skills/Skills";
-import Experience from "./components/Experience/Experience";
 import Work from "./components/Work/Work";
-import Education from "./components/Education/Education";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
-import BlurBlob from "./components/Blurblob";
+import BlurBlob from "./components/BlurBlob/BlurBlob";
+ // Correct path with .jsx
+// if file is src/components/BlurBlob.jsx
+
+
 
 const App = () => {
   return (
@@ -36,15 +38,7 @@ const App = () => {
         </section>
 
         <section className="pt-16">
-          <Experience />
-        </section>
-
-        <section className="pt-16">
           <Work />
-        </section>
-
-        <section className="pt-16">
-          <Education />
         </section>
 
         <section className="pt-16">
