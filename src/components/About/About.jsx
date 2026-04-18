d16c1967169835f3ea850be7ffe2c2a34cfadfa4
Delta Student <hp3432884@gmail.com>
2026-04-18 23:08:43 +0530

    update

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -257,7 +257,7 @@ const About = () => {
             <div className="flex items-start gap-4">
               <FaCube className="text-yellow-400 mt-1" />
               <p>
-                Solved <span className="text-yellow-400">200+ LeetCode</span> problems.
+                Solved <span className="text-yellow-400">250+ LeetCode</span> problems.
               </p>
             </div>
 
@@ -285,7 +285,7 @@ const About = () => {
             </a>
 
             <a
-              href="https://drive.google.com/file/d/1kBEEZOE28jAj-QHBeW01bYfFxP0PSQkE/view?usp=drivesdk"
+              href="https://drive.google.com/file/d/1jSGvcMmKcQMHnuhB5cBOmH9oSUb3zXSO/view?usp=sharing"
               target="_blank"
               rel="noopener noreferrer"
               className="px-6 md:px-8 py-3 rounded-full border border-purple-500 
@@ -307,7 +307,7 @@ const About = () => {
         <div className="flex flex-col md:flex-row justify-between items-center text-center gap-8">
 
           <div className="flex-1">
-            <h3 className="text-2xl md:text-3xl font-bold text-purple-400">200+</h3>
+            <h3 className="text-2xl md:text-3xl font-bold text-purple-400">250+</h3>
             <p className="text-gray-400 text-sm md:text-base">LeetCode Problems</p>
           </div>
 
